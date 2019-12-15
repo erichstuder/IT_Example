342c85676be89629707789ae8bfab5d08c23450d
erichstuder <richu_@hotmail.com>
2019-12-15 09:32:22 +0100

    initial creation of build file

---
+++ build.bat
@@ -0,0 +1,18 @@
+echo off
+
+echo Building library for pytest with MSBuild
+"C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\MSBuild\Current\Bin\MSBuild.exe" "%~dp0\source\deviceWithServer\itLibrary\itLibrary.sln" /property:Configuration=Release >nul 2>&1
+if errorlevel 1 (
+	echo MSBuild failed
+) else (
+	echo MSBuild success
+)
+
+echo Execute pytest
+pytest "%~dp0\source\client\tests" >nul 2>&1
+if errorlevel 1 (
+	echo pytest failed
+) else (
+	echo pytest success
+)
+
