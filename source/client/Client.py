2ad215d34b4b8215e751c9e0c24067a1c494e46c
erichstuder <richu_@hotmail.com>
2020-02-15 14:09:57 +0100

    modifications and updated tests

--- source/client/Client.py
+++ source/client/Client.py
@@ -16,166 +16,72 @@ You should have received a copy of the GNU Affero General Public License
 along with this program.  If not, see <https://www.gnu.org/licenses/>.
 """
 
-# import time
-# import serial  # install pyserial to gain access
-# import sys
-# import os
-import queue
-from app.ComPortHandler import ComPortHandler
-# from app.TextFileViewer import TextFileViewer
-# import app.Preferences as Preferences
-# from app.TelegramParser import TelegramParser
+from app.ComportHandler import ComportHandler
 import threading
 import time
-# from datetime import datetime
-
-"""
-def handleComPortInput(data):
-    try:
-        print(data.decode("utf-8"), end='')
-    except UnicodeDecodeError:
-        print('')
-        print("Not utf-8: ", end='')
-        print(data)
-"""
+import os
 
 
 class Client:
     def __init__(self):
-        self.__keyboardInputQueue = queue.Queue()
+        self.__running = True
+        self.__comPortHandler = ComportHandler()
         self.__keyboardReaderThread = threading.Thread(target=self.__keyboardReaderWorker)
         self.__keyboardReaderThread.daemon = True
         self.__keyboardReaderThread.start()
-        self.__running = True
-        self.__comPortHandler = ComPortHandler()
 
     def __keyboardReaderWorker(self):
-        while True:
-            self.__keyboardInputQueue.put(input())
-
-    # only for testing at the moment
-    def getKeyboardInputQueue(self):
-        return self.__keyboardInputQueue
+        while self.__running:
+            self.__keyboardInputParser(input().strip())
 
     def run(self):
         while self.__running:
-            while not self.__keyboardInputQueue.empty():
-                self.__keyboardInputParser(self.__keyboardInputQueue.get())
-
             while True:
                 data = self.__comPortHandler.read()
                 if data is not None:
-                    with open('mySession.session', 'a+b') as sessionFile:
+                    with open("mySession.session", "a+b") as sessionFile:
                         sessionFile.write(data)
                 else:
                     break
-
             time.sleep(0.1)
 
-    def __keyboardInputParser(self, keyBoardInput):
-        if keyBoardInput == 'list comports':
+    def __keyboardInputParser(self, keyboardInput):
+        """ list comports may be activated in future
+        if keyboardInput == 'list comports':
             self.__comPortHandler.getFriendlyNames()
-        elif keyBoardInput.startswith('start'):
-            self.__comPortHandler.configure(port='COM4', baudrate=9600)
-            print('started')
-        elif keyBoardInput == 'exit':
+        el
+        """
+        if keyboardInput.startswith('set comport '):
+            comPort = keyboardInput.split(' ')[2]
+            self.__comPortHandler.setPort(port=comPort)
+            self.__printAnswer('comport set to: ' + comPort)
+        elif keyboardInput.startswith('set baudrate '):
+            baudrate = keyboardInput.split(' ')[2]
+            self.__comPortHandler.setBaudrate(baudrate=baudrate)
+            self.__printAnswer('baudrate set to: ' + baudrate)
+        elif keyboardInput.startswith('run '):
+            scriptFileName = keyboardInput.split(' ')[1]
+            if os.path.isfile(scriptFileName):
+                self.__printAnswer('running: ' + scriptFileName)
+                with open(scriptFileName, 'r') as scriptFile:
+                    for line in scriptFile:
+                        self.__keyboardInputParser(line.strip())
+            else:
+                self.__printAnswer('error: file not found')
+        elif keyboardInput == 'exit':
+            self.__printAnswer('goodbye...')
             self.__running = False
+            time.sleep(0.5)
         else:
-            self.__comPortHandler.write(keyBoardInput + '\r')
-            print('sent: ' + keyBoardInput)
-            # send to server
+            self.__comPortHandler.write(keyboardInput + "\r")
+            self.__printAnswer('sent to server: ' + keyboardInput)
+
+    @staticmethod
+    def __printAnswer(answer):
+        print('  ' + answer)
 
 
 if __name__ == "__main__":
+    os.system('mode 70,15')
+    os.system("title IT client")
     Client().run()
-
-# def __telegramReceived(telegram):
-#     with open(activeSession, 'a+') as logFile:
-#         logFile.write(datetime.today().strftime('%d.%m.%Y %H:%M:%S\n'))
-#         logFile.write(str(telegram) + '\n')
-#         logFile.write(telegram['valueName'] + ': ' + str(telegram['value']) + '\n\n')
-
-
-# def __invalidTelegram(telegram):
-#     with open(activeSession, 'a+') as logFile:
-#         logFile.write('invalid:\n')
-#         logFile.write(''.join('{:02x} '.format(x) for x in telegram))
-#         logFile.write('\n')
-#         logFile.write(str(bytes(telegram)))
-#         logFile.write('\n')
-#         logFile.write('\n')
-
-
-
-
-"""
-comPortReceiver = ComPortReceiver(port='COM6', baudrate=9600)
-comPortReceiver.setOnConnecting(lambda: print('comPortReceiver connecting'))
-comPortReceiver.setOnConnected(lambda: print('comPortReceiver connected'))
-comPortReceiver.setOnReceived(handleComPortInput)
-comPortReceiver.setOnError(lambda e: print('comPortReceiver error: ' + e))
-comPortReceiver.setOnErrorNoExit(lambda: print('comPortReceiver errorNoExit'))
-comPortReceiver.start()
-"""
-
-
-
-
-"""
-comPortHandler = ComPortHandler(port='COM8', baudrate=9600)
-telegramParser = TelegramParser(telegramReceivedCallback=__telegramReceived, invalidTelegramCallback=__invalidTelegram)
-"""
-
-"""
-preferences = Preferences.load('preferences.txt')
-activeSession = preferences['activeSession']
-if not os.path.exists(activeSession):
-    open(activeSession, 'a').close()
-TextFileViewer(activeSession)
-while True:
-    while not inputQueue.empty():
-        inputData = inputQueue.get(block=False)
-        inputData = inputData + '\r'
-        comPortHandler.write(inputData)
-
-    data = comPortHandler.read()
-    if data is not None:
-        telegramParser.parse(data)
-    # time.sleep(0.01)
-"""
-
-"""
-    try:
-        data = comPortHandler.read()
-        print(data, end='')
-    except UnicodeDecodeError:
-        print('')
-        print("Not utf-8: ", end='')
-        print(data)
-    time.sleep(0.01)
-    """
-
-"""
-    cmd = input()
-    if cmd == 'exit':
-        print('goodbye ...')
-        sys.stdout.flush()
-        time.sleep(2)
-        break
-    else:
-        # serialPort = serial.Serial(port='COM6', exclusive=False)
-        # serialPort.baudrate = 9600
-        # serialPort.write(cmd)
-        print(cmd)
-    """
-
-"""
-    elif cmd == 'status':
-        print('status not yet implemented')
-    elif cmd == 'start':
-        print('start not yet implemented')
-    elif cmd == 'stop':
-        print('stop not yet implemented')
-    else:
-        print(cmd)
-    """
--- source/client/tests/Client_test.py
+++ source/client/tests/Client_test.py
@@ -16,27 +16,176 @@ You should have received a copy of the GNU Affero General Public License
 along with this program.  If not, see <https://www.gnu.org/licenses/>.
 """
 
-import Client as Client
-import time
-# from unittest.mock import Mock
-# from unittest.mock import patch
-# import os
-# from datetime import datetime
-
-"""
-test list:
-- senden an com port
-
-"""
+import Client
+import builtins
 
 
-def test_inputFromKeyboard(mocker):
-    textFromInput = 'This is a test. \n'
-    keyboardInput_mocked = mocker.patch('builtins.input', return_value=textFromInput)
+def test_constructor(mocker):
+    comportHandler_mock = mocker.patch("Client.ComportHandler")
+
+    mockManager = mocker.Mock()
+    mockManager.attach_mock(comportHandler_mock, "comportHandler_mock")
+
     client = Client.Client()
-    time.sleep(0.1)  # give enough time to read from keyboard
-    keyboardInput_mocked.assert_called()
-    a = client.getKeyboardInputQueue().get()
-    assert client.getKeyboardInputQueue().get() == textFromInput
+
+    expectedCallOrder = [
+        mocker.call.comportHandler_mock(),
+    ]
+    assert mockManager.mock_calls == expectedCallOrder
 
 
+def test_setComport(mocker):
+    comport = "COM23"
+    input_mock = mocker.patch("builtins.input", side_effect=["set comport " + comport + "\n", "exit\n"])
+    print_mock = mocker.patch("builtins.print")
+    comportHandler_setPort_mock = mocker.patch("Client.ComportHandler.setPort")
+
+    mockManager = mocker.Mock()
+    mockManager.attach_mock(input_mock, "input_mock")
+    mockManager.attach_mock(print_mock, "print_mock")
+    mockManager.attach_mock(comportHandler_setPort_mock, "comportHandler_setPort_mock")
+
+    client = Client.Client()
+    client.run()
+
+    expectedCallOrder = [
+        mocker.call.input_mock(),
+        mocker.call.comportHandler_setPort_mock(port=comport),
+        mocker.call.print_mock("  comport set to: " + comport),
+        mocker.call.input_mock(),
+        mocker.call.print_mock("  goodbye..."),
+    ]
+    assert mockManager.mock_calls == expectedCallOrder
+
+
+def test_setBaudrate(mocker):
+    baudrate = "1234"
+    input_mock = mocker.patch("builtins.input", side_effect=["set baudrate " + baudrate + "\n", "exit\n"])
+    print_mock = mocker.patch("builtins.print")
+    comportHandler_setBaudrate_mock = mocker.patch("Client.ComportHandler.setBaudrate")
+
+    mockManager = mocker.Mock()
+    mockManager.attach_mock(input_mock, "input_mock")
+    mockManager.attach_mock(print_mock, "print_mock")
+    mockManager.attach_mock(comportHandler_setBaudrate_mock, "comportHandler_setBaudrate_mock")
+
+    client = Client.Client()
+    client.run()
+
+    expectedCallOrder = [
+        mocker.call.input_mock(),
+        mocker.call.comportHandler_setBaudrate_mock(baudrate=baudrate),
+        mocker.call.print_mock("  baudrate set to: " + baudrate),
+        mocker.call.input_mock(),
+        mocker.call.print_mock("  goodbye..."),
+    ]
+    assert mockManager.mock_calls == expectedCallOrder
+
+
+def test_runNonexistentFile(mocker):
+    input_mock = mocker.patch("builtins.input", side_effect=["run myTextNonExistent.txt\n", "exit\n"])
+    print_mock = mocker.patch("builtins.print")
+
+    mockManager = mocker.Mock()
+    mockManager.attach_mock(input_mock, "input_mock")
+    mockManager.attach_mock(print_mock, "print_mock")
+
+    client = Client.Client()
+    client.run()
+
+    expectedCallOrder = [
+        mocker.call.input_mock(),
+        mocker.call.print_mock("  error: file not found"),
+        mocker.call.input_mock(),
+        mocker.call.print_mock("  goodbye..."),
+    ]
+    assert mockManager.mock_calls == expectedCallOrder
+
+
+def test_runFile(mocker, tmpdir):
+    textFile = "myText.txt"
+    with open(textFile, "w") as file:
+        file.write("exit\n")
+
+    input_mock = mocker.patch("builtins.input", side_effect=["run " + textFile + "\n"])
+    print_mock = mocker.patch("builtins.print")
+
+    mockManager = mocker.Mock()
+    mockManager.attach_mock(input_mock, "input_mock")
+    mockManager.attach_mock(print_mock, "print_mock")
+
+    client = Client.Client()
+    client.run()
+
+    expectedCallOrder = [
+        mocker.call.input_mock(),
+        mocker.call.print_mock("  running: myText.txt"),
+        mocker.call.print_mock("  goodbye..."),
+    ]
+    assert mockManager.mock_calls == expectedCallOrder
+
+
+def test_exit(mocker):
+    input_mock = mocker.patch("builtins.input", side_effect=["exit\n"])
+    print_mock = mocker.patch("builtins.print")
+
+    mockManager = mocker.Mock()
+    mockManager.attach_mock(input_mock, "input_mock")
+    mockManager.attach_mock(print_mock, "print_mock")
+
+    client = Client.Client()
+    client.run()
+
+    expectedCallOrder = [
+        mocker.call.input_mock(),
+        mocker.call.print_mock("  goodbye..."),
+    ]
+    assert mockManager.mock_calls == expectedCallOrder
+
+
+def test_sendToServer(mocker):
+    cmdToServer = "blabla"
+    input_mock = mocker.patch("builtins.input", side_effect=[cmdToServer + "\n", "exit\n"])
+    print_mock = mocker.patch("builtins.print")
+    comportHandler_write_mock = mocker.patch("Client.ComportHandler.write")
+
+    mockManager = mocker.Mock()
+    mockManager.attach_mock(input_mock, "input_mock")
+    mockManager.attach_mock(print_mock, "print_mock")
+    mockManager.attach_mock(comportHandler_write_mock, "comportHandler_write_mock")
+
+    client = Client.Client()
+    client.run()
+
+    expectedCallOrder = [
+        mocker.call.input_mock(),
+        mocker.call.comportHandler_write_mock(cmdToServer + "\r"),
+        mocker.call.print_mock("  sent to server: " + cmdToServer),
+        mocker.call.input_mock(),
+        mocker.call.print_mock("  goodbye..."),
+    ]
+    assert mockManager.mock_calls == expectedCallOrder
+
+
+def test_nothingToReadFromServer(mocker):
+    mocker.patch("builtins.input", return_value="exit\n")
+    mocker.patch("Client.ComportHandler.read", return_value=None)
+    mocker.patch("builtins.open")
+
+    client = Client.Client()
+    client.run()
+
+    Client.ComportHandler.read.assert_called_with()
+    builtins.open.assert_not_called()
+
+
+def test_readFromServer(mocker):
+    mocker.patch("builtins.input", return_value="exit\n")
+    mocker.patch("Client.ComportHandler.read", side_effect=[42, None])
+    mocker.patch("builtins.open")
+
+    client = Client.Client()
+    client.run()
+
+    Client.ComportHandler.read.assert_called_with()
+    builtins.open.assert_called_with("mySession.session", "a+b")
