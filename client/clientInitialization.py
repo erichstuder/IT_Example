3630a77120f9d6745bbda566692f0f02d4425029
erichstuder <richu_@hotmail.com>
2021-08-15 20:32:17 +0200

    infrastructur to run client

---
+++ client/clientInitialization.py
@@ -0,0 +1,5 @@
+send('set connectionType USB_RS232')
+send('set VID 2341')
+send('set PID 8036')
+send('reset')
+
---
+++ client/run.sh
@@ -0,0 +1,2 @@
+IT_Client/env/bin/python3 IT_Client/Client.py -initFile clientInitialization.py
+
