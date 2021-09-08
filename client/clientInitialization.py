278f1e6b23cc251d474f7404561bb55318cfdf0a
erichstuder <richu_@hotmail.com>
2021-09-08 09:31:33 +0200

    more information for the user

--- client/clientInitialization.py
+++ client/clientInitialization.py
@@ -1,7 +1,12 @@
+def mySend(text):
+	print('send: "' +  text + '"\n')
+	send(text)   
+
+
 send('set connectionType USB_RS232')
 send('set VID 2341')
 send('set PID 8036')
-send('reset')
-send('log desiredValue')
-send('log actualValue')
 
+mySend('reset')
+mySend('log desiredValue')
+mySend('log actualValue')
